0700c1a734164b4f92de66563c87d406d7324655
eaftada.tasin69@gmail.com <eaftada.tasin69@gmail.com>
2025-05-21 13:58:42 +0000

    Edited QFL

--- client_code/QFL/__init__.py
+++ client_code/QFL/__init__.py
@@ -1,5 +1,11 @@
 from ._anvil_designer import QFLTemplate
 from anvil import *
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+!pip install mpltern
+import mpltern
+import re
 
 class QFL(QFLTemplate):
   def __init__(self, **properties):
@@ -8,10 +14,33 @@ class QFL(QFLTemplate):
 
     # Any code you write here will run before the form opens.
 
+  def clean_commas(input_string):
+    cleaned_string = re.sub(r',+', ',', input_string)
+    if cleaned_string.endswith(','):
+      cleaned_string = cleaned_string[:-1]
+    return cleaned_string
+
   def button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
     self.InputArea.visible = not self.InputArea.visible
 
-  def INPUTarea(self, **event_args):
-    """This method is called when the column panel is shown on the screen"""
-    
+  def SubmitButton_click(self, **event_args):
+    AaQ_str = input("Enter Quartz values separated by commas (,): ")
+    AaF_str = input("Enter Feldspar values separated by commas (,): ")
+    AaL_str = input("Enter Lithics values separated by commas (,): ")
+
+    try:
+    # Quartz
+     cleaned_q = clean_commas(AaQ_str)
+     qList = [int(x.strip()) for x in cleaned_q.split(',') if x.strip()]
+
+    # Feldspar
+     cleaned_f = clean_commas(AaF_str)
+     fList = [int(x.strip()) for x in cleaned_f.split(',') if x.strip()]
+
+    # Lithics
+     cleaned_l = clean_commas(AaL_str)
+     lList = [int(x.strip()) for x in cleaned_l.split(',') if x.strip()]
+
+    except ValueError:
+     print("Invalid input. Please enter numerical values separated by commas.")
