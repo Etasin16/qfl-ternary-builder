a90d81369a3574db94ea51c72f06d4a155c05287
eaftada.tasin69@gmail.com <eaftada.tasin69@gmail.com>
2025-05-21 13:27:57 +0000

    Edited QFL

--- client_code/QFL/__init__.py
+++ client_code/QFL/__init__.py
@@ -7,3 +7,11 @@ class QFL(QFLTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.InputArea.visible = not self.InputArea.visible
+
+  def INPUTarea(self, **event_args):
+    """This method is called when the column panel is shown on the screen"""
+    
