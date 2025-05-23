f714cacf6a0247901408dad7260c65048794772e
eaftada.tasin69@gmail.com <eaftada.tasin69@gmail.com>
2025-05-23 17:50:33 +0000

    Edited ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -1,5 +1,5 @@
 import anvil.server
-anvil.server.connect("server_JZOCNSMKQ47PWJCHFIFMA7JI-GNGZTW2B2JR4ERZQ")
+anvil.server.connect(server_JZOCNSMKQ47PWJCHFIFMA7JI-GNGZTW2B2JR4ERZQ)
 import anvil.files
 from anvil.files import data_files
 import anvil.tables as tables
@@ -10,12 +10,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import mpltern
 
-
-
 # This is a server module. It runs on the Anvil server,
 # rather than in the user's browser.
 #
-# To allow anvil.server.call() to call functions here, we mark
+
 # them with @anvil.server.callable.
 # Here is an example - you can replace it with your own:
 #
